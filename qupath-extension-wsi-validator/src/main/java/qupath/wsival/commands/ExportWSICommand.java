a6f7fddc72a5fb5beb6d111f4dcb2563024dd0b2
Tuatini Godard <tuatinigodard@gmail.com>
2018-09-11 12:18:41 +0200

    Add validate as Reviewer in Admin mode

--- qupath-extension-wsi-validator/src/main/java/qupath/wsival/commands/ExportWSICommand.java
+++ qupath-extension-wsi-validator/src/main/java/qupath/wsival/commands/ExportWSICommand.java
@@ -24,8 +24,7 @@ public class ExportWSICommand implements PathCommand {
         this.qupath = qupath;
     }
 
-    private void saveValidatedMeta(ProjectImageEntry<BufferedImage> wsi) {
-        QuPathGUI.UserProfileChoice userChoice = qupath.getUserProfileChoice();
+    private void saveValidatedMeta(ProjectImageEntry<BufferedImage> wsi, QuPathGUI.UserProfileChoice userChoice) {
         Map<String, String> meta = new HashMap<>(wsi.getMetadataMap());
 
         meta.put(QuPathGUI.WSI_VALIDATED, userChoice.name());
@@ -76,37 +75,40 @@ public class ExportWSICommand implements PathCommand {
         if (currentChoice == QuPathGUI.UserProfileChoice.REVIEWER_MODE ||
                 currentChoice == QuPathGUI.UserProfileChoice.ADMIN_MODE) {
 
-            String firstChoice = currentChoice == QuPathGUI.UserProfileChoice.ADMIN_MODE ?
-                    "Reset validation" : "Validate WSI annotation";
-            String[] choices = {firstChoice,
-                    "Give to " + QuPathGUI.UserProfileChoice.SPECIALIST_MODE,
-                    "Give to " + QuPathGUI.UserProfileChoice.CONTRACTOR_MODE,
-                    "Give to " + QuPathGUI.UserProfileChoice.REVIEWER_MODE};
+            String firstChoiceName = currentChoice == QuPathGUI.UserProfileChoice.ADMIN_MODE ?
+                    "Validate the WSI annotations as a Reviewer" : "Validate the WSI annotations";
+            List<String> choices = new ArrayList<>();
+            choices.add(firstChoiceName);
+            choices.add("Give to " + QuPathGUI.UserProfileChoice.SPECIALIST_MODE);
+            choices.add("Give to " + QuPathGUI.UserProfileChoice.CONTRACTOR_MODE);
+            choices.add("Give to " + QuPathGUI.UserProfileChoice.REVIEWER_MODE);
+            if (currentChoice == QuPathGUI.UserProfileChoice.ADMIN_MODE) {
+                choices.add("Reset validation (equivalent to give to " +
+                        QuPathGUI.UserProfileChoice.SPECIALIST_MODE + ")");
+            }
             String curVal = wsi.getMetadataMap().get(QuPathGUI.WSI_VALIDATED) == null ? "No one" :
                     QuPathGUI.UserProfileChoice.valueOf(wsi.getMetadataMap().get(QuPathGUI.WSI_VALIDATED)).toString();
-            String choice = DisplayHelpers.showChoiceDialog("Change validation ownership",
+            String choice = (String) DisplayHelpers.showChoiceDialog("Change validation ownership",
                     "In " + currentChoice + " the validation button allow you to reassign the " +
                             "\nWSI to a user mode.\n" +
                             "Please select to whom you want to give the WSI:\n\nCurrently validated by: "
-                            + curVal, choices, choices[0]);
+                            + curVal, choices.toArray(), choices.get(0));
             if (choice != null) {
-                if (choice.equals(choices[0])) {
-                    if (currentChoice == QuPathGUI.UserProfileChoice.ADMIN_MODE) {
-                        reassignValidationOwnership(wsi, null);
-                    } else {
-                        saveValidatedMeta(wsi);
-                    }
-                } else if (choice.equals(choices[1])) {
+                if (choice.equals(choices.get(0))) {
+                    reassignValidationOwnership(wsi, QuPathGUI.UserProfileChoice.REVIEWER_MODE);
+                } else if (choice.equals(choices.get(1))) {
                     reassignValidationOwnership(wsi, null);
-                } else if (choice.equals(choices[2])) {
+                } else if (choice.equals(choices.get(2))) {
                     reassignValidationOwnership(wsi, QuPathGUI.UserProfileChoice.SPECIALIST_MODE);
-                } else if (choice.equals(choices[3])) {
+                } else if (choice.equals(choices.get(3))) {
                     reassignValidationOwnership(wsi, QuPathGUI.UserProfileChoice.CONTRACTOR_MODE);
+                } else if (choice.equals(choices.get(4))) {
+                    reassignValidationOwnership(wsi, null);
                 }
             }
         } else {
             if (getConfirmation()) {
-                saveValidatedMeta(wsi);
+                saveValidatedMeta(wsi, currentChoice);
             }
         }
     }
